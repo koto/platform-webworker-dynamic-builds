6d304cedd52ca745dedf4f2796a4f6e8380724c0
Krzysztof Kotowicz <koto@google.com>
2019-03-22 15:20:28 +0000

    e4c2d298a fix(platform-browser): Linter fixes.

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @license Angular v8.0.0-beta.8+60.sha-e4c2d29
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ */
+export declare const VERSION: Version;
+
+export { }
